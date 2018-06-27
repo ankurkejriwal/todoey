50555411d51d805c6f1c45b5c4274e02719dfede
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2018-06-27 00:32:10 -0400

    Persisting data using Codable Protocol and Plist

--- todoey/Controllers/ToDoListViewController.swift
+++ todoey/Controllers/ToDoListViewController.swift
@@ -10,30 +10,34 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike","Buy Eggos", "Destroy the Thinker"]
+    var itemArray = [customDataModel]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
     
-    let defaults = UserDefaults.standard// Stores key value pairs consistently
-    
-    //let defaults2 = UserDefaults.standard
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let item = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = item
-        }
+       // print (dataFilePath)
+        let newItem = customDataModel()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
         
-        else if  let item2 = defaults.array(forKey: "motherTrucker") as? [String]{
-            itemArray = item2
-        }
+        loadItems()
+        
+        
         
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) // Populating the current index path
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        let itemz = itemArray[indexPath.row]
+
+        cell.accessoryType = itemz.checkmark == true ? .checkmark : .none //Ternary Operator
+        
         
         return cell
     }
@@ -46,16 +50,12 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print (itemArray[indexPath.row])
+         itemArray[indexPath.row].checkmark = !itemArray[indexPath.row].checkmark
+        
+       saveItems()
         
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }
         
-        else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
@@ -69,11 +69,12 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             
-            self.itemArray.append(textField.text!)
-            //what would happen when the user clicks the add item button on our UI Alert
+            let newItem = customDataModel()
+            newItem.title = textField.text!
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData()
+            self.itemArray.append(newItem)
+            
+           self.saveItems()
         
         }
         
@@ -96,14 +97,50 @@ class ToDoListViewController: UITableViewController {
         let action2 = UIAlertAction(title: "Delete all entries", style: .default) { (action) in
             self.itemArray.removeAll()
             
-            self.defaults.set(self.itemArray, forKey: "motherTrucker")
-           
-            self.tableView.reloadData()
+           self.saveItems()
             
             
         }
         alert2.addAction(action2)
         present(alert2, animated: true, completion: nil)
+        
+        
+    }
+    
+    func saveItems (){
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!) //Writing data to a data file path
+        }
+            
+        catch {
+            print("error encoding item array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems(){
+        
+        if let data =  try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+            do{
+                itemArray = try decoder.decode([customDataModel].self, from: data)
+                
+            }
+            
+            catch {
+                print("error encoding item array, \(error)")
+            }
+        }
+        
+        
+        
+        
+        
         
         
     }
---
+++ todoey/Data Model/CusomDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  CusomDataModel.swift
+//  todoey
+//
+//  Created by Ankur Kejriwal on 2018-06-16.
+//  Copyright © 2018 Ankur kejriwal. All rights reserved.
+//
+
+import Foundation
+
+
+class customDataModel: Codable{
+    
+    var title : String = ""
+    var checkmark : Bool = false
+}
